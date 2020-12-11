f3dc7f460af93a571eb50c35f127e492015121ba
DAM110 <DAM110@IF03-11.iesmhp.local>
2020-12-11 17:36:11 +0100

    Apartado A

--- src/examen_daniel_dehesa/Examen_Daniel_Dehesa.java
+++ src/examen_daniel_dehesa/Examen_Daniel_Dehesa.java
@@ -5,6 +5,8 @@
  */
 package examen_daniel_dehesa;
 
+import java.util.Scanner;
+
 /**
  *
  * @author DAM110
@@ -16,6 +18,35 @@ public class Examen_Daniel_Dehesa {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner teclado = new Scanner(System.in);
+        boolean salir = false;
+        do {
+            System.out.println("1. Cambio de unidades de horas a segundos.");
+            System.out.println("2. Cambio de unidades de kilomentros a metros.");
+            System.out.println("3. Salir");
+
+            System.out.println("Escribe una de las opciones");
+            int opcion = teclado.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    System.out.println("Introduce las horas a cambiar: ");
+                    int horas = teclado.nextInt();
+                    int s = horas * 3600;
+                    System.out.println(horas + " horas son " + s + " segundos.");
+                    break;
+                case 2:
+                    System.out.println("Introduce los kilometros a cambiar");
+                    int km = teclado.nextInt();
+                    int m = km * 1000;
+                    System.out.println(km + " km son " + m + " metros.");
+                    break;
+                case 3:
+                    salir = true;
+                    break;
+                default:
+                    System.out.println("Solo números entre 1 y 3");
+            }
+        } while (salir == false);
     }
-    
 }
